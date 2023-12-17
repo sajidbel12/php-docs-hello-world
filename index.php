4154fc7e28121b398c87d0432f16e04095ea2c76
unknown <nizareuser>
2023-12-17 18:12:35 +0100

    updated output

--- index.php
+++ index.php
@@ -3,18 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Réservation de Salles - Connexion</title>
-    <link rel="stylesheet" href="style.css">
+    <title>Réservation de voitures</title>
+    <link rel="stylesheet" href="style.css"> <!-- Update the CSS file link -->
 </head>
 <body>
-    <form action="loginWork.php" method="post">
-        <h2>Connexion</h2>
-        <label for="username">Nom d'utilisateur :</label>
-        <input type="text" id="username" name="username" required>
-        <label for="password">Mot de passe :</label>
-        <input type="password" id="password" name="password" required>
-        <button type="submit">Se Connecter</button>
-    </form>
+    <div class="container">
+        <form action="loginWork.php" method="post" class="styled-form">
+            <h1>Connexion</h1>
+            <div class="form-group">
+                <label for="username">Nom d'utilisateur :</label>
+                <input type="text" id="username" name="username" required>
+            </div>
+            <div class="form-group">
+                <label for="password">Mot de passe :</label>
+                <input type="password" id="password" name="password" required>
+            </div>
+            <button type="submit">Se Connecter</button>
+        </form>
+    </div>
 </body>
 </html>
-
