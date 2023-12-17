bea07edb1b0e61a90e4624f1e54f43580de4c5cc
unknown <nizareuser>
2023-12-17 17:31:52 +0100

    updated output

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Réservation de Salles - Connexion</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <form action="logicLogin.php" method="post">
+        <h2>Connexion</h2>
+        <label for="username">Nom d'utilisateur :</label>
+        <input type="text" id="username" name="username" required>
+        <label for="password">Mot de passe :</label>
+        <input type="password" id="password" name="password" required>
+        <button type="submit">Se Connecter</button>
+    </form>
+</body>
+</html>
 
-echo "Hello cloud!";
